5729ff02c9b55a00d55dd9fabda45b46d9008612
randomize <randomize46@gmail.com>
2014-05-10 11:45:39 +0300

    blblblb

---
+++ Assets/FolowChick.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FolowChick : MonoBehaviour {
+
+	public float upperBound = 0.1f; 
+	public float LowerBound = 0.1f; 
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Paralax.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[UnityEngine.ExecuteInEditMode]
+public class Paralax : MonoBehaviour {
+
+	[Range(0.01f, 5)]
+	public float obj;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position = new Vector3(transform.position.x, Camera.main.transform.position.y * obj);
+
+
+	
+	}
+}
