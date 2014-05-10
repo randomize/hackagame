3fc62fa35a38685553c1282ffe36379b36d5c3e8
nick <fuduley@gmail.com>
2014-05-10 18:18:04 +0300

    bullet

--- Assets/BulletScript.cs
+++ Assets/BulletScript.cs
@@ -5,8 +5,12 @@ public class BulletScript : MonoBehaviour {
 	public Vector3 speed;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Awake () {
+		BulletUpd ();
+		//transform.localRotation = Quaternion.AngleAxis (0, speed);
+			//FromToRotation (Vector3.zero, speed);
+
+
 	}
 	
 	// Update is called once per frame
@@ -15,5 +19,21 @@ public class BulletScript : MonoBehaviour {
 		t += speed * Time.deltaTime;
 
 		transform.position = t;
+
+		if (t.x < -40 || t.x > 40){
+			BulletUpd();
+		}
+	}
+	void BulletUpd() {
+		transform.position = 
+			new Vector3 (Random.Range (0, 2) == 0 ? 30 : -30, Random.Range (0, 100), 0);
+		speed = 5 * new Vector3 (Random.Range (-1, 1f), Random.Range (-1, 1f));
+		var angle = Mathf.Atan2(speed.y, speed.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle +180));
+	}
+	void OnTriggerEnter2D(Collider2D other) {
+		//play delink
+		//AudioSource.PlayClipAtPoint (delink, transform.position);
+		Destroy(gameObject);
 	}
 }
--- Assets/BulletSpawner.cs
+++ Assets/BulletSpawner.cs
@@ -1,15 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class BulletSpawner : MonoBehaviour {
 
+	public int count = 0;
+	public int rate = 30;
+
 	// Use this for initialization
-	void Start () {
-	
-	}
+	/*void Start () {
+		for (int i = 0; i< count; i++) {
+			var bulletInstance = Resources.Load("bullet");
+			var pos = new Vector3(0, 0, 0);
+			Instantiate( bulletInstance, 
+			                       pos+transform.position, 
+			                       Quaternion.identity ) as GameObject;
+		}
+	}*/
 	
 	// Update is called once per frame
 	void Update () {
-	
+		var currPosition = Camera.main.transform.position;
+		int c = (int) (currPosition.y / rate - count);
+		for (int i = 0; i < c; i++) {
+			var bulletInstance = Resources.Load("bullet");
+			var pos = new Vector3(0, 0, 0);
+			var g = Instantiate( bulletInstance, 
+			            pos+transform.position, 
+			            Quaternion.identity ) as GameObject;
+		}
+
+		count += c;
 	}
 }
